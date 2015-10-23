e13c5fef71fc91eacbc724aef1de3dfaa547f7e1
2dos <2dossrl@gmail.com>
2015-10-22 21:11:02 -0500

    The Actual Counter
    oh look 2dos FINALLY pushed it up

---
+++ WpfApplication1/IncrementWindow.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfApplication1
+{
+    public partial class IncrementWindow : Form
+    {
+        MainWindow mw = null;
+        int temp;
+
+        public IncrementWindow(MainWindow mw)
+        {
+            InitializeComponent();
+            this.mw = mw;
+		}
+
+		private void plusGB_Click(object sender, EventArgs e)
+		{
+            temp = this.mw.GB;
+            if(temp < 201)
+            {
+			    temp++;
+			    this.mw.ValueGB(temp);
+			    this.mw.Load();
+			}
+		}
+
+        private void minusGB_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.GB;
+            if (temp > 0)
+            {
+                temp--;
+                this.mw.ValueGB(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void plusBlueprint_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.BP;
+            if (temp < 40)
+            {
+                temp++;
+                this.mw.ValueBP(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void minusBlueprint_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.BP;
+            if (temp > 0)
+            {
+                temp--;
+                this.mw.ValueBP(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void plusMedal_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.Medal;
+            if (temp < 40)
+            {
+                temp++;
+                this.mw.ValueMedal(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void minusMedal_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.Medal;
+            if (temp > 0)
+            {
+                temp--;
+                this.mw.ValueMedal(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void plusFairy_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.Fairy;
+            if (temp < 20)
+            {
+                temp++;
+                this.mw.ValueFairy(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void minusFairy_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.Fairy;
+            if (temp > 0)
+            {
+                temp--;
+                this.mw.ValueFairy(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void plusCrown_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.Crown;
+            if (temp < 10)
+            {
+                temp++;
+                this.mw.ValueCrown(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void minusCrown_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.Crown;
+            if (temp > 0)
+            {
+                temp--;
+                this.mw.ValueCrown(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void plusKey_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.BossKey;
+            if (temp < 8)
+            {
+                temp++;
+                this.mw.ValueKey(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void minusKey_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.BossKey;
+            if (temp > 0)
+            {
+                temp--;
+                this.mw.ValueKey(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void plusN64Coin_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.N64Coin;
+            if (temp < 1)
+            {
+                temp++;
+                this.mw.ValueN64Coin(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void minusN64Coin_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.N64Coin;
+            if (temp > 0)
+            {
+                temp--;
+                this.mw.ValueN64Coin(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void plusRWCoin_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.RWCoin;
+            if (temp < 1)
+            {
+                temp++;
+                this.mw.ValueRWCoin(temp);
+                this.mw.Load();
+            }
+        }
+
+        private void minusRWCoin_Click(object sender, EventArgs e)
+        {
+            temp = this.mw.RWCoin;
+            if (temp > 0)
+            {
+                temp--;
+                this.mw.ValueRWCoin(temp);
+                this.mw.Load();
+            }
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int GB = 0;
+        public int BP = 0;
+        public int Medal = 0;
+        public int Fairy = 0;
+        public int Crown = 0;
+        public int BossKey = 0;
+        public int N64Coin = 0;
+        public int RWCoin = 0;
+        public int Percentage = 0;
+        public decimal actualPercent = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Load()
+        {
+            numGB.Text = GB.ToString();
+            numBP.Text = BP.ToString();
+            numMedal.Text = Medal.ToString();
+            numFairy.Text = Fairy.ToString();
+            numCrown.Text = Crown.ToString();
+            numKey.Text = BossKey.ToString();
+            numN64Coin.Text = N64Coin.ToString();
+            numRWCoin.Text = RWCoin.ToString();
+            CalcPercent();
+            numPercent.Text = Percentage.ToString();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void ShowWindow_Click(object sender, RoutedEventArgs e)
+        {
+            IncrementWindow iw = new IncrementWindow(this);
+            iw.Show();
+        }
+
+        private void PercentInfo_Click(object sender, RoutedEventArgs e)
+        {
+            PercentInfo pi = new PercentInfo();
+            pi.Show();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void CalcPercent()
+        {
+            actualPercent = Convert.ToDecimal((GB * 0.4) + (Medal * 0.2) + (Fairy * 0.2) + (Crown * 0.5) + (BossKey * 0.25) + (N64Coin * 0.5) + (RWCoin * 0.5));
+            Percentage = Decimal.ToInt32(actualPercent);
+            if (actualPercent.Equals(100.4m))
+            {
+                Percentage = 101;
+            }
+        }
+
+        public void ValueGB(int nG)
+        {
+            GB = nG;
+        }
+
+        public void ValueBP(int nB)
+        {
+            BP = nB;
+        }
+
+        public void ValueMedal(int nM)
+        {
+            Medal = nM;
+        }
+
+        public void ValueFairy(int nF)
+        {
+            Fairy = nF;
+        }
+
+        public void ValueCrown(int nC)
+        {
+            Crown = nC;
+        }
+
+        public void ValueKey(int nK)
+        {
+            BossKey = nK;
+        }
+
+        public void ValueN64Coin(int nN)
+        {
+            N64Coin = nN;
+        }
+
+        public void ValueRWCoin(int nR)
+        {
+            RWCoin = nR;
+        }
+    }
+}
